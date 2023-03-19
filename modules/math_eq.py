2104808a4906a753cf5abf479f5b85e11dd918eb
Fedor Lebed <flebedev777@gmail.com>
2023-03-19 10:49:20 +0300

    added new formulas

--- modules/math_eq.py
+++ modules/math_eq.py
@@ -1,3 +1,14 @@
+import numpy as np
+import requests
+
+
 def count_velocity(v_max, w_engine, weight):
     return v_max * (w_engine / 80) * (200 / weight)
 
+
+def count_population_coefficient(temperature, oxygen):
+    return np.sin(-np.pi / 2 + np.pi * (temperature + 0.5 * oxygen) / 40)
+
+
+def count_new_population(population, coefficient):
+    return population + coefficient * population
