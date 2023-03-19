d545fe2fc7559dc42d6a4623c4405e62d3c183b5
OlejaCat <118455092+OlejaCat@users.noreply.github.com>
2023-03-19 13:11:40 +0300

    sigma_2

--- modules/app.py
+++ modules/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, request, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,10 +14,26 @@ def index():
     return render_template('token.html')
 
 
-@app.route('/token/<string:token>')
+class EditPointForm(FlaskForm):
+    SH = StringField('SH')
+    fuel = StringField('Топливо')
+
+class SmolForm(FlaskForm):
+    point = StringField('point')
+
+
+@app.route('/token/<string:token>', methods=['GET', 'POST'])
 def count_page(token):
     # load data from data base
-    return 'Count Page'
+    form = EditPointForm(SH=120)
+    form_1 = SmolForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print(2)
+        if form_1.validate_on_submit():
+            print(1)
+        # update data
+    return render_template('info.html', form=form, form_1=form_1)
 
 
 if __name__ == '__main__':
