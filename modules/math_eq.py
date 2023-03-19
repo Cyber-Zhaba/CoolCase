d75ac97cecfac03ca68f4407d7ecaadd831a1086
mav735 <mav735@ya.ru>
2023-03-19 07:05:08 +0300

    added structure, test_py

---
+++ modules/math_eq.py
@@ -0,0 +1,3 @@
+def count_velocity(v_max, w_engine, weight):
+    return v_max * (w_engine / 80) * (200 / weight)
+
---
+++ test/test_equation.py
@@ -0,0 +1,11 @@
+import pytest
+from modules.math_eq import count_velocity
+
+
+class TestClass:
+    def test_count_velocity(self):
+        v_max = 50
+        w_engine_percent = 20
+        weight = 100
+
+        assert count_velocity(v_max, w_engine_percent, weight) == 25
